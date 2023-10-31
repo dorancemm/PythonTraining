458d5527bf329345467b65aa0735569b3ed58c1f
Dorance Montoya <dorancemm@gmail.com>
2023-10-31 15:41:23 +0100

    Example Mass–energy equivalence

---
+++ einstein.py
@@ -0,0 +1,8 @@
+# Input: Enter the mass in Kilograms
+masa_kg = int(input("Enter the mass in Kilograms: ")) 
+
+#Calculate equation: e = mc^2
+c = 3000000
+e = masa_kg * c * c  
+
+print ("The energy is equal to", e, "joules.")
